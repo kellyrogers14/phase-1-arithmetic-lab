72570e896a4fc349d30e8a5aa95dd0d2ae812115
Kelly Rogers <kellyerogers92@gmail.com>
2023-11-07 17:34:10 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = (num1)*(num2);
+console.log(multiply);
+
+//
+
+const random = Math.round(Math.random()* 1000);
+console.log(random);
+
+//
+
+const num3 = 10;
+const num4 = 6;
+const mod = (num3)-(num4);
+console.log(mod);
+
+//
+
+const max = Math.max(5,10,15,20)
